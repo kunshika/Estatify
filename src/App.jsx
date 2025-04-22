b3cb923f711bc834f2c4ac99070055db5eff7b94
kunshika <kunshika1803@gmail.com>
2025-04-23 00:07:29 +0530

    create pages and commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter ,Routes,Route }   from 'react-router-dom';
+import Home from './Pages/Home';
+import SignIn from './Pages/Signin';
+import SignUp from './Pages/SignUp';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+
 
 export default function App() {
-  return <h1 className='text-red-500'> hello </h1>
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={< SignUp/>} />
+    <Route path="/about" element={< About/>} />
+    <Route path="/profile" element={< Profile/>} />
+</Routes>
+</BrowserRouter>
   
 }
 

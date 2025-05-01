97f2ba2655ffe9798775ca35edf9a24444954f7e
kunshika <kunshika1803@gmail.com>
2025-05-01 12:35:55 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+// ✅ Named export
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async(req, res) => {
+  // your logic here
+   const {username,email,password}=req.body;
+   const hashedPassword=bcrypt.hashSync(password,10);//this is the way to hash a password in bcryptjs
+   const newUser=new User({username,email,password:hashedPassword});//this is the way to create a new user in the database 
+   try{
+    await newUser.save();//this is the way to save a new user in the database
+    res.status(200).json({message:"User created successfully!"});//this is the way to send a response to the client
+   }catch(err){
+    res.status(500).json({message:"User creation failed!"});//this is the way to send a response to the client
+   }
+
+   
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("MongoDB connected successfully!");
@@ -11,7 +12,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log("MongoDB connection failed!",err);
 });
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("Server is running on port 3000!")
 })
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);

9c910f2c4e80d49b332cc1da7ba467dfcee52878
kunshika <kunshika1803@gmail.com>
2025-05-15 18:48:05 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async(req, res,next) => {
   // your logic here
@@ -19,3 +20,22 @@ export const signup = async(req, res,next) => {
 
    
 };
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+      const validUser=await User.findOne({email});
+      if(!validUser){
+        return next(errorHandler(404,"User not found!"));//this is the way to send an error to the error handling middleware
+      }
+      const validPassword=bcrypt.compareSync(password,validUser.password);
+      if(!validPassword){
+        return next(errorHandler(401,"Invalid credentials!"));//this is the way to send an error to the error handling middleware
+      }
+      const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);//this is the way to create a token using jwt
+      const {password:pass,...rest}=validUser._doc;//this is the way to destructure an object in javascript
+      res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);//this is the way to send a response to the client
+    }catch(err){
+      next(err);
+    }
+}
